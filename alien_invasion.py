32bb2f7215b45d9df058b493364b14589dc31c25
Wenni <liang_wen_yan@163.com>
2019-02-13 23:06:51 +0800

    实现ship基本移动

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+# 初始化游戏并创建一个屏幕对象
+def run_game():
+
+    # 初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    # screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    # 设置背景色
+    # bg_color = (230, 230, 230)
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+
+        # 每次循环时都重绘屏幕
+        # screen.fill(bg_color)
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
