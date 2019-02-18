fa3a8b929661f07cb740a06dfc65e3b96e83ee9e
Wenni <liang_wen_yan@163.com>
2019-02-18 23:46:51 +0800

    添加外星人并实现其相关功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -15,17 +16,23 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
-    # 创建一个用于存储子弹的编组
+    # 创建外星人
+    alien = Alien(ai_settings, screen)
+    # 创建一个用于存储子弹的编组、存储外星人的编组
     bullets = Group()
+    aliens = Group()
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        # print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,20 +1,26 @@
 # 要修改游戏，只需修改settings.py中的一些值
 
+
 class Settings():
     """存储《外星人入侵》的所有设置的类"""
 
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕设置
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # 飞船设置
         self.ship_speed_factor = 1.5
         # 子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 10
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示外星人向右移，-1表示向左移动
+        self.fleet_direction = 1
 
